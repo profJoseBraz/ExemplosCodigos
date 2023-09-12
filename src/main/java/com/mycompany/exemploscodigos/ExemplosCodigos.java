afbcff09c3eae246e38282a82a88cfc08885c0b3
Jose Augusto Braz <jose.braz@docente.pr.senac.br>
2023-09-12 08:50:16 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/exemploscodigos/ExemplosCodigos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemploscodigos;
+
+/**
+ *
+ * @author 10156
+ */
+public class ExemplosCodigos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
